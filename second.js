c4014efaaee569d4fe1b1c8ac92afb2995ac11e4
Akash0000001 <akashranjan947@gmail.com>
2023-05-25 17:27:32 +0530

    using this keyword

--- second.js
+++ second.js
@@ -3,6 +3,19 @@ let product =(x,y)=>{
 }
 console.log(product(2,3))
 
+let student={
+    name:"Akash",
+    marks:80,
+    age:25,
+    getplacement(minage,minmarks)
+    {
+        if(this.marks>=minmarks && this.age>=minage)
+            return this.name+" is eligible for placements."
+        else
+            return this.name+" is not eligible for placements."    
+    }
+}
+console.log(student.getplacement(18,40))
 
 
 
